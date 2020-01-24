b3b0a755cca87ec8aa933b00e9ca1f05d965a55a
Willis Aybar <willisaybar@gmail.com>
2020-01-24 01:56:11 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(language="Squawk!", name="Pretty bird!")
+  puts "#{language}"
+  "Squawk!"
+  "#{language}"
+end
