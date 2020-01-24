a5663e26b1cb9e6dc5d0427ed8645316411c5fe7
Willis Aybar <willisaybar@gmail.com>
2020-01-24 01:19:51 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(pretty_bird=name, squawk=sound)
+def parrot(name="Pretty Bird!", sound="Squawk!")
   "Squawk!"
 end
 
