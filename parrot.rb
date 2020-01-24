b5536681ca736be7abb2fcc1e0cd3e9bf927f9f9
Willis Aybar <willisaybar@gmail.com>
2020-01-24 01:25:27 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(bird="Pretty Bird", sound="Squawk!")
-  puts "A parrot is a #{name}!"
-end
-
-def greeting(name="Ruby programmer", language="Ruby")
-  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+def parrot(name="Pretty Bird!", language="Squawk!")
+  puts "#{name} #{language}"
 end
   
